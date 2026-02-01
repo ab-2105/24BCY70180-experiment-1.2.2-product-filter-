28a0f053df518921ebb320347936260f9f8ad2ca
ab-2105 <bansaladitya507@gmail.com>
2026-02-01 18:03:36 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
+const textarea = document.getElementById('myText');
+const countDisplay = document.getElementById('count');
 
-document.addEventListener('DOMContentLoaded', () => {
-    const dropdown = document.getElementById('sort-dropdown');
-    const productList = document.getElementById('product-list');
-    const products = Array.from(document.getElementsByClassName('product-card'));
-
-    dropdown.addEventListener('change', function() {
-        const sortValue = this.value;
-
-        const sorted = products.sort((a, b) => {
-            const priceA = parseInt(a.getAttribute('data-price'));
-            const priceB = parseInt(b.getAttribute('data-price'));
-
-            if (sortValue === 'low') return priceA - priceB;
-            if (sortValue === 'high') return priceB - priceA;
-            return 0;
-        });
-
-        sorted.forEach(card => productList.appendChild(card));
-    });
+// Listen for typing events
+textarea.addEventListener('input', () => {
+    // Set the text of the span to the length of the textarea value
+    countDisplay.textContent = textarea.value.length;
 });
